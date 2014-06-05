84f3c879e800ba15ca06101e42b804d780ee66ab
FracturedRetina <admin@krakendev.net>
2014-06-05 15:43:33 -0700

    Added score popup

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 var score = 0;
+var scoreChanges = 0;
 
 $(document).ready(function() {
 	//If player is new
@@ -12,15 +13,15 @@ $(document).ready(function() {
 	}
 });
 
-$('canvas').click(function(e) {
-	var x = e.clientX - $(this).offset().left;
-	var y = e.clientY - $(this).offset().top;
+$('canvas, .scoreText').click(function(e) {
+	var x = e.clientX - $('#cvs').offset().left;
+	var y = e.clientY - $('#cvs').offset().top;
 	var distFromCent = Math.sqrt(
 		Math.pow(x - centX, 2)
 		+Math.pow(y - centY, 2)
 	);
 	var scoreChange;
-	
+
 	var angle = normalize(540 - (
 		Math.atan2(
 			x - centX,
@@ -42,44 +43,51 @@ $('canvas').click(function(e) {
 		var d = new Date();
 		var timeMultiplier = 1000 * (1 / (d.getTime() - drawTime));
 		
-		
+
 		console.log("Offset: " + getOffset(angle, targetAngle));
 		scoreChange = Math.round(timeMultiplier * (getOffset(angle, normalize(targetAngle + 180)) - 90));
 		score += scoreChange;
 		$('#score').text("Score: " + score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-		
+
+		//Save highscore and lowscore
+		if (score > $.cookie("highscore")) {
+			$.cookie("highscore", score, {expires: 365});
+		} else if (score < $.cookie("lowscore")) {
+			$.cookie("lowscore", score, {expires: 365});
+		}
+
 		console.log("Click valid!");
 	} else {
 		console.log("Click invalid!");
 	}
-	
-	
+	scoreChanges++;
+
+	//Show point text
+	if (scoreChange > 0) {
+		$("body").append("<p id=\"sc" + scoreChanges + "\" class=\"scoreText green\" style=\"top: " + e.clientY + "px; left: " + e.clientX + "px;\">+" + scoreChange +  "</p>")
+	} else if (scoreChange < 0) {
+		$("body").append("<p id=\"sc" + scoreChanges + "\" class=\"scoreText red\" style=\"top: " + e.clientY + "px; left: " + e.clientX + "px;\">" + scoreChange + "</p>")
+	}
+
+	$('#sc' + scoreChanges).slideDown(400).delay(1000).slideUp(400);
+
+
 	drawBG();
 	targetAngle = drawAntiTarget();
 });
 
-
-//Save highscore and lowscore
-$(window).bind('beforeunload', function() {
-	if (score > $.cookie("highscore")) {
-		$.cookie("highscore", score, {expires: 365});
-	} else if (score < $.cookie("lowscore")) {
-		$.cookie("lowscore", score, {expires: 365});
-	}
-});
-
 function getOffset(angle1, angle2) {
 	var offset;
-	
+
 	if (angle1 > angle2) {
 		offset = angle1 - angle2;
 	} else {
 		offset = angle2 - angle1;
 	}
-	
+
 	while (offset > 180) {
 		offset = 360 - offset;
 	}
-	
+
 	return offset;
 }
